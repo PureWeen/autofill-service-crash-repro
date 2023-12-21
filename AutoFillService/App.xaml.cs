3daac8e4693393f5dedde689b57af6bc88a39cf6
Shane Neuville <shneuvil@microsoft.com>
2023-12-21 17:14:30 -0600

    - add some main page variations

--- AutoFillService/App.xaml.cs
+++ AutoFillService/App.xaml.cs
@@ -12,9 +12,73 @@ namespace AutoFillService
         protected override Window CreateWindow(IActivationState activationState)
         {
             if (activationState.State.Count > 0)
-                 return new Window(new NavigationPage(new ContentPage() { Content = new Label() { Text = "Auto Fill service page!" } }));
+            {
+                var userApplicaitonWindow = Windows.OfType<UserApplicationWindow>().FirstOrDefault();
 
-            return new Window(new NavigationPage(new MainPage()));
+                if (userApplicaitonWindow is not null)
+                {
+                    userApplicaitonWindow.PendingPage = new NavigationPage(new ContentPage() { Content = new Label() { Text = "reseting page on main page!" } });
+                }
+
+                 return new AutoFillWindow(new ContentPage() { Content = new Label() { Text = "Auto Fill service page!" } });
+            }
+
+
+            var autofillWindow = Windows.OfType<AutoFillWindow>().FirstOrDefault();
+            if (autofillWindow is not null)
+            {
+                autofillWindow.PendingPage = new NavigationPage(new ContentPage() { Content = new Label() { Text = "User opened main app" } });
+                // or you could close the autofill window here if you wanted to
+            }
+
+            return new UserApplicationWindow(new NavigationPage(new MainPage()));
         }
+    }
+
+    public class ResumeHackWindow : Window
+    {
+        public Page PendingPage {get;set;}
+
+        public ResumeHackWindow(Page page) : base(page)
+        {
+
+        }
+
+        /// <summary>
+        /// You need to do this inside OnActivated not OnResumed
+        /// Androids OnResume maps to OnActivated
+        /// Androids OnRestart is what Maps to OnResumed
+        /// I realize this is confusing from the perspective of Android
+        /// https://github.com/dotnet/maui/issues/1720 explains it a bit better
+        /// </summary>
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+
+            // This shouldn't be necessary. I'll need to investigate why you can't just set the page during this call
+           // await Task.Yield();
+            if (PendingPage is not null)
+                Page = PendingPage;
+
+            PendingPage = null;
+        }
+    }
+    
+    public class UserApplicationWindow : ResumeHackWindow
+    {
+
+        public UserApplicationWindow(Page page) : base(page)
+        {
+
+        }
+    }
+
+    public class AutoFillWindow : ResumeHackWindow
+    {       
+        public AutoFillWindow(Page page) : base(page)
+        {
+
+        }
+
     }
 }
